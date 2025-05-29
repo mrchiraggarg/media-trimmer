3feb0110c9fd5780112f8f90eb57a2c7f2e3af5e
mrchiraggarg <gargchirag728@gmail.com>
2025-05-29 22:22:45 +0530

    changes updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        clay: '10px 10px 20px #c8d0e7, -10px -10px 20px #ffffff',
+        clayInset: 'inset 6px 6px 10px #c8d0e7, inset -6px -6px 10px #ffffff',
+      },
+      borderRadius: {
+        clay: '1.5rem',
+      },
+    },
   },
   plugins: [],
-}
+};
